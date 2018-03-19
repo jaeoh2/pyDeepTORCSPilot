091774b2973b8740edf3146ffae518892e89654e
Jaeoh Lee <suby38@gmail.com>
2018-03-19 11:07:26 +0900

    Update simu.cpp fix the build error

--- pyDeepTORCSPilot/vtorcs/src/modules/simu/simuv2/simu.cpp
+++ pyDeepTORCSPilot/vtorcs/src/modules/simu/simuv2/simu.cpp
@@ -61,7 +61,7 @@ ctrlCheck(tCar *car)
     if (isnan(car->ctrl->brakeCmd) || isinf(car->ctrl->brakeCmd)) car->ctrl->brakeCmd = 0;
     if (isnan(car->ctrl->clutchCmd) || isinf(car->ctrl->clutchCmd)) car->ctrl->clutchCmd = 0;
     if (isnan(car->ctrl->steer) || isinf(car->ctrl->steer)) car->ctrl->steer = 0;
-    if (isnan(car->ctrl->gear) || isinf(car->ctrl->gear)) car->ctrl->gear = 0;
+    //if (isnan(car->ctrl->gear) || isinf(car->ctrl->gear)) car->ctrl->gear = 0;
 #else
     if (isnan(car->ctrl->accelCmd)) car->ctrl->accelCmd = 0;
     if (isnan(car->ctrl->brakeCmd)) car->ctrl->brakeCmd = 0;
