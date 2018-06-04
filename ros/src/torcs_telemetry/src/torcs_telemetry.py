4f1f95f7188a8b6dcbc45eaf04373ea23f4282d8
jaeoh2 <suby38@gmail.com>
2018-06-04 23:27:51 +0900

    Update torcs_telemetry pub and init drive_control function

--- ros/src/torcs_telemetry/src/torcs_telemetry.py
+++ ros/src/torcs_telemetry/src/torcs_telemetry.py
@@ -20,55 +20,13 @@ import struct
 import os
 import sys
 import time
-from pynput.keyboard import Key, Controller
-
-rospy.init_node('torcs_telemetry', anonymous=True)
-pub = rospy.Publisher('torcs_telemetry',
-                      TORCSSensors,
-                      queue_size=10
-                      )
-rate = rospy.Rate(100)
 
 data_size = 2**17
-
 
 def clip(v,lo,hi):
     if v<lo: return lo
     elif v>hi: return hi
     else: return v
-
-
-def bargraph(x,mn,mx,w,c='X'):
-    '''Draws a simple asciiart bar graph. Very handy for
-    visualizing what's going on with the data.
-    x= Value from sensor, mn= minimum plottable value,
-    mx= maximum plottable value, w= width of plot in chars,
-    c= the character to plot with.'''
-    if not w: return '' # No width!
-    if x<mn: x= mn      # Clip to bounds.
-    if x>mx: x= mx      # Clip to bounds.
-    tx= mx-mn # Total real units possible to show on graph.
-    if tx<=0: return 'backwards' # Stupid bounds.
-    upw= tx/float(w) # X Units per output char width.
-    if upw<=0: return 'what?' # Don't let this happen.
-    negpu, pospu, negnonpu, posnonpu= 0,0,0,0
-    if mn < 0: # Then there is a negative part to graph.
-        if x < 0: # And the plot is on the negative side.
-            negpu= -x + min(0,mx)
-            negnonpu= -mn + x
-        else: # Plot is on pos. Neg side is empty.
-            negnonpu= -mn + min(0,mx) # But still show some empty neg.
-    if mx > 0: # There is a positive part to the graph
-        if x > 0: # And the plot is on the positive side.
-            pospu= x - max(0,mn)
-            posnonpu= mx - x
-        else: # Plot is on neg. Pos side is empty.
-            posnonpu= mx - max(0,mn) # But still show some empty pos.
-    nnc= int(negnonpu/upw)*'-'
-    npc= int(negpu/upw)*c
-    ppc= int(pospu/upw)*c
-    pnc= int(posnonpu/upw)*'_'
-    return '[%s]' % (nnc+npc+ppc+pnc)
 
 
 def destringify(s):
@@ -109,8 +67,17 @@ class Client(object):
         if t: self.trackname = t
         if s: self.stage = s
         if d: self.debug = d
+
+        self.pub = rospy.Publisher('torcs_sensors',
+                              TORCSSensors,
+                              queue_size=10
+                              )
+        self.sub = rospy.Subscriber('torcs_ctrl', TORCSCtrl, self.drive_control)
+        self.rate = rospy.Rate(100)
+
         self.S = ServerState()
         self.R = DriverAction()
+
         self.setup_connection()
 
     def setup_connection(self):
@@ -143,39 +110,12 @@ class Client(object):
                 sockdata = sockdata.decode('utf-8')
             except socket.error as emsg:
                 print("Waiting for server on %d............" % self.port)
-                print("Count Down : " + str(n_fail))
-                if n_fail < 0:
-                    print("relaunch torcs")
-                    os.system('pkill torcs')
-                    time.sleep(1.0)
-                    if self.vision is False:
-                        os.system('torcs -nofuel -nodamage -nolaptime &')
-                    else:
-                        os.system('torcs -nofuel -nodamage -nolaptime -vision &')
-
-                    time.sleep(1.0)
-                    # os.system('sh autostart.sh')
-                    keyboard = Controller()
-                    keyboard.press(Key.enter)
-                    keyboard.release(Key.enter)
-                    keyboard.press(Key.enter)
-                    keyboard.release(Key.enter)
-                    keyboard.press(Key.up)
-                    keyboard.release(Key.up)
-                    keyboard.press(Key.up)
-                    keyboard.release(Key.up)
-                    keyboard.press(Key.enter)
-                    keyboard.release(Key.enter)
-                    keyboard.press(Key.enter)
-                    keyboard.release(Key.enter)
-
-                    n_fail = 5
-                n_fail -= 1
 
             identify = '***identified***'
             if identify in sockdata:
                 print("Client connected on %d.............." % self.port)
                 break
+
 
     def get_servers_input(self):
         '''Server's input is stored in a ServerState object'''
@@ -235,8 +175,8 @@ class Client(object):
                 tos.wheelSpinVel =self.S.d['wheelSpinVel']
                 tos.z = self.S.d['z']
 
-                pub.publish(tos)
-                rate.sleep()
+                self.pub.publish(tos)
+                self.rate.sleep()
 
                 # rospy.loginfo(sockdata)
                 if self.debug:
@@ -244,18 +184,25 @@ class Client(object):
                     print(self.S)
                 break  # Can now return from this function.
 
+
     def respond_to_server(self):
         if not self.so: return
         try:
             message = repr(self.R)
             self.so.sendto(message.encode(), (self.host, self.port))
+
         except socket.error as emsg:
             print("Error sending to server: %s Message %s" % (emsg[1], str(emsg[0])))
-            sys.exit(-1)
-        if self.debug: print(self.R.fancyout())
-        rospy.loginfo(self.R.fancyout())
-        # Or use this for plain output:
-        # if self.debug: print self.R
+
+
+    def drive_control(self, torcs_ctrl):
+        if not self.so: return
+
+        self.R['steer'] = torcs_ctrl.steering
+        self.R['accel'] = torcs_ctrl.accel
+        self.R['gear'] = torcs_ctrl.gear
+        self.R['brake'] = torcs_ctrl.brake
+
 
     def shutdown(self):
         if not self.so: return
@@ -331,24 +278,6 @@ class DriverAction():
         return out
         return out+'\n'
 
-    def fancyout(self):
-        '''Specialty output for useful monitoring of bot's effectors.'''
-        out= str()
-        od= self.d.copy()
-        od.pop('gear','') # Not interesting.
-        od.pop('meta','') # Not interesting.
-        od.pop('focus','') # Not interesting. Yet.
-        for k in sorted(od):
-            if k == 'clutch' or k == 'brake' or k == 'accel':
-                strout=''
-                strout= '%6.3f %s' % (od[k], bargraph(od[k],0,1,50,k[0].upper()))
-            elif k == 'steer': # Reverse the graph to make sense.
-                strout= '%6.3f %s' % (od[k], bargraph(od[k]*-1,-1,1,50,'S'))
-            else:
-                strout= str(od[k])
-            out+= "%s: %s\n" % (k,strout)
-        return out
-
 
 def drive_example(c):
     '''This is only an example. It will get around the track but the
@@ -389,19 +318,18 @@ def drive_example(c):
     return
 
 
-def pub_telemetry():
-    pass
-
-
 if __name__ == '__main__':
     try:
+        rospy.init_node('torcs_telemetry', anonymous=True)
         client = Client(p=3001)
 
         while not rospy.is_shutdown():
             # ToDo: Implement server status publisher
+            # rospy.spin()
             client.get_servers_input()
             drive_example(client)
             client.respond_to_server()
+            # rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
