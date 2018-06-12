9487b375496c2fb81af9e9c18058d5f058f1d2ec
jaeoh2 <suby38@gmail.com>
2018-06-12 10:26:01 +0900

    update torcs_img_pub with mss module and cv_brgidge

--- ros/src/torcs_img_pub/src/torcs_img_pub.py
+++ ros/src/torcs_img_pub/src/torcs_img_pub.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import Image, CompressedImage
+from torcs_msgs.msg import TORCSCtrl, TORCSSensors
+from cv_bridge import CvBridge
 
+import numpy as np
+import mss
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(20) # 20Hz
+monitor = {'top': 40, 'left': 0, 'width': 800, 'height': 640}
+
+def img_pub():
+    pub = rospy.Publisher('/image_color', Image, queue_size=1)
+    cbridge = CvBridge()
+    rospy.init_node('torcs_ros_img_publisher', anonymous=True)
+    rate = rospy.Rate(30) # Hz
     while not rospy.is_shutdown():
-        hell_str = "hell world {}".format(rospy.get_time())
-        rospy.loginfo(hell_str)
-        pub.publish(hell_str)
+        with mss.mss() as sct:
+            img = sct.grab(monitor)
+
+	ros_img = cbridge.cv2_to_imgmsg(np.asarray(img), 'rgba8')
+        pub.publish(ros_img)
+
         rate.sleep()
 
 
 if __name__ == "__main__":
     try:
-        talker()
+        img_pub()
 
     except rospy.ROSInterruptException:
         pass
