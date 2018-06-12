ccc7cb1f6acff785ba750a9e3370d7fe71f8ff0b
jaeoh2 <suby38@gmail.com>
2018-06-12 10:41:57 +0900

    fix screen grap position torcs_img_pub

--- ros/src/torcs_img_pub/src/torcs_img_pub.py
+++ ros/src/torcs_img_pub/src/torcs_img_pub.py
@@ -8,7 +8,7 @@ from cv_bridge import CvBridge
 import numpy as np
 import mss
 
-monitor = {'top': 60, 'left': 0, 'width': 800, 'height': 660}
+monitor = {'top': 60, 'left': 0, 'width': 800, 'height': 590}
 
 def img_pub():
     pub = rospy.Publisher('/image_color', Image, queue_size=1)
